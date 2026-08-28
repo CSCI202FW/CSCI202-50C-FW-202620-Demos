096c854939d5dd2e0660130334e4675579711aed
Angela Venable <avenable11@ivytech.edu>
2026-08-27 20:33:31 -0400

    start of classes and pointers demo

---
+++ 08_27_26(pointers review)/donut.h
@@ -0,0 +1,73 @@
+#ifndef DONUT_H
+#define DONUT_H
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <algorithm>
+
+enum class icingType
+{
+    CHOCOLATEICING,
+    CARAMELICING,
+    MAPLE,
+    VANILLA,
+    GLAZE,
+    SPECIALICE,
+    NOICE
+};
+
+enum drizzleType
+{
+    CARAMELDRIZZLE,
+    CHOCOLATEDRIZZLE,
+    PEANUTBUTTER,
+    RASPBERRY,
+    SPECIALDRIZZLE,
+    NODRIZZLE
+};
+class Donut
+{
+public:
+    enum toppingType
+    {
+        RAINBOWSPR,
+        CHOCOLATESPR,
+        PEANUTS,
+        COCONUT,
+        OREO,
+        BACON,
+        GRAHAM,
+        FRUITYPEBBLES,
+        REESESPIECES,
+        MMS,
+        CANDYBAR,
+        CHOCOLATECHIPS,
+        SEASALT,
+        CINSUGAR,
+        POWDSUGAR,
+        NOTOP
+    };
+    Donut(std::string icing = "none", std::string topping = "none", std::string drizzle = "none");
+    icingType getIcing() const;
+    toppingType getTopping() const;
+    drizzleType getDrizzle() const;
+    void setIcing(std::string icing);
+    void setTopping(std::string topping);
+    void setDrizzle(std::string drizzle);
+    std::string toString() const;
+    friend std::ostream &operator<<(std::ostream &, const Donut &);
+    const static std::map<icingType, std::string> iceToStr;
+    const static std::map<std::string, icingType> strToice;
+    const static std::map<drizzleType, std::string> drizzleToStr;
+    const static std::map<std::string, drizzleType> strToDrizzle;
+    const static std::map<toppingType, std::string> topToStr;
+    const static std::map<std::string, toppingType> strToTop;
+
+private:
+    icingType icing;
+    toppingType topping;
+    drizzleType drizzle;
+};
+
+#endif
