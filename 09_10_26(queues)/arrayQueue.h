32c3d05b339885abd223080510c609c573190fb2
Angela Venable <avenable11@ivytech.edu>
2026-09-10 20:12:34 -0400

    both queue's implemented testing needed.

--- 09_10_26(queues)/arrayQueue.h
+++ 09_10_26(queues)/arrayQueue.h
@@ -1,6 +1,7 @@
 #ifndef ARRAYQ_H
 #define ARRAYQ_H
 #include "queueADT.h"
+#include <stdexcept>
 template <class t>
 class ArrayQueue : QueueADT<t>
 {
@@ -25,4 +26,145 @@ private:
     int count;
     void copyQueue(const ArrayQueue<t> &queueToCopy);
 };
+
+template <class t>
+void ArrayQueue<t>::enqueue(const t &queueElement)
+{
+    if (isFullQueue())
+    {
+        throw std::overflow_error("Cannot add to a full queue.");
+    }
+    queueRear = (queueRear + 1) % maxQueueSize;
+    list[queueRear] = new t(queueElement);
+    count++;
+}
+
+template <class t>
+t ArrayQueue<t>::dequeue()
+{
+    if (isEmptyQueue())
+    {
+        throw std::out_of_range("Cannot delete from an empty queue.");
+    }
+    queueFront = (queueFront + 1) % maxQueueSize;
+    t copy = *(list[queueFront]);
+    delete list[queueFront];
+    list[queueFront] = nullptr;
+    count--;
+    return copy;
+}
+
+template <class t>
+void ArrayQueue<t>::copyQueue(const ArrayQueue<t> &queueToCopy)
+{
+    if (!isEmptyQueue())
+    {
+        this->initializeQueue();
+    }
+    if (maxQueueSize < queueToCopy.maxQueueSize)
+    {
+        delete[] list;
+        maxQueueSize = queueToCopy.maxQueueSize;
+        list = new t *[maxQueueSize];
+        for (int i = 0; i < this->maxQueueSize; i++)
+        {
+            list[i] = nullptr;
+        }
+    }
+    queueFront = maxQueueSize - 1;
+    queueRear = maxQueueSize - 1;
+    for (int i = (queueToCopy.queueFront + 1) % queueToCopy.maxQueueSize; i != (queueRear + 1) % queueToCopy.maxQueueSize; i = (i + 1) % queueToCopy.maxQueueSize)
+    {
+        enqueue(*(queueToCopy.list[i]));
+    }
+}
+
+template <class t>
+ArrayQueue<t>::ArrayQueue(int max)
+{
+    if (max < 0)
+    {
+        max = QueueADT<t>::DEFAULT_QUEUE_SIZE;
+    }
+    maxQueueSize = max;
+    list = new t *[maxQueueSize];
+    for (int i = 0; i < maxQueueSize; i++)
+    {
+        list[i] = nullptr;
+    }
+    count = 0;
+    queueFront = maxQueueSize - 1;
+    queueRear = maxQueueSize - 1;
+}
+
+template <class t>
+ArrayQueue<t>::ArrayQueue(const ArrayQueue<t> &queueToCopy)
+{
+    list = nullptr;
+    maxQueueSize = 0;
+    queueFront = queueRear = maxQueueSize - 1;
+    count = 0;
+    copyQueue(queueToCopy);
+}
+
+template <class t>
+const ArrayQueue<t> &ArrayQueue<t>::operator=(const ArrayQueue<t> &queueToCopy)
+{
+    if (this != &queueToCopy)
+    {
+        copyQueue(queueToCopy);
+    }
+    return *this;
+}
+
+template <class t>
+ArrayQueue<t>::~ArrayQueue()
+{
+    initializeQueue();
+    delete[] list;
+}
+
+template <class t>
+bool ArrayQueue<t>::isEmptyQueue() const
+{
+    return count == 0 || queueFront == queueRear;
+}
+
+template <class t>
+bool ArrayQueue<t>::isFullQueue() const
+{
+    return count == maxQueueSize || (queueRear + 1) % maxQueueSize == queueFront;
+}
+
+template <class t>
+void ArrayQueue<t>::initializeQueue()
+{
+    for (int i = (queueFront + 1) % maxQueueSize; i != queueRear; i = (i + 1) % maxQueueSize)
+    {
+        delete list[i];
+        list[i] = nullptr;
+    }
+    queueFront = queueRear = maxQueueSize - 1;
+    count = 0;
+}
+
+template <class t>
+t ArrayQueue<t>::front() const
+{
+    if (isEmptyQueue())
+    {
+        throw std::out_of_range("Cannot get the first item of an empty queue.");
+    }
+    return *(list[(queueFront + 1) % maxQueueSize]);
+}
+
+template <class t>
+t ArrayQueue<t>::back() const
+{
+    if (isEmptyQueue())
+    {
+        throw std::out_of_range("Cannot get the last item of an empty queue.");
+    }
+    return *(list[queueRear]);
+}
 #endif
--- 09_10_26(queues)/linkedQueue.h
+++ 09_10_26(queues)/linkedQueue.h
@@ -2,6 +2,7 @@
 #define LINKEDQUEUE_H
 #include "queueADT.h"
 #include "node.h"
+#include <stdexcept>
 template <class t>
 class LinkedQueue : public QueueADT<t>
 {
@@ -23,4 +24,145 @@ private:
     Node<t> *queueRear;
     void copyQueue(const LinkedQueue<t> &queueToCopy);
 };
+
+template <class t>
+void LinkedQueue<t>::enqueue(const t &queueElement)
+{
+    Node<t> *newNode;
+    newNode = new Node(queueElement);
+    if (this->isEmptyQueue())
+    {
+        this->queueFront = newNode;
+        this->queueRear = newNode;
+    }
+    else
+    {
+        this->queueRear->link = newNode; // put the new item on teh end of the list
+        this->queueRear = newNode;       // move tail to the new last node
+    }
+}
+
+template <class t>
+t LinkedQueue<t>::dequeue()
+{
+    if (isEmptyQueue())
+    {
+        throw std::out_of_range("Cannot delete from an empty queue.");
+    }
+    Node<t> *temp;
+    temp = queueFront;
+    queueFront = queueFront->link;
+    t copy(temp->data);
+    delete temp;
+    return copy;
+}
+
+template <class t>
+void LinkedQueue<t>::copyQueue(const LinkedQueue<t> &queueToCopy)
+{
+    Node<t> *newNode;
+    Node<t> *current;
+    if (!isEmptyQueue())
+    {
+        initializeQueue();
+    }
+    if (queueToCopy.isEmptyQueue())
+    {
+        queueFront = nullptr;
+        queueRear = nullptr;
+    }
+    else
+    {
+        current = queueToCopy.queueFront;
+        queueFront = new Node<t>(current->data);
+        queueRear = queueFront;
+        current = current->link;
+        while (current != nullptr)
+        {
+            newNode = new Node<t>(current->data);
+            queueRear->link = newNode;
+            queueRear = newNode;
+            current = current->link;
+        }
+    }
+}
+
+template <class t>
+LinkedQueue<t>::LinkedQueue()
+{
+    queueFront = nullptr;
+    queueRear = nullptr;
+}
+
+template <class t>
+LinkedQueue<t>::LinkedQueue(const LinkedQueue<t> &queueToCopy)
+{
+    queueRear = nullptr;
+    queueFront = nullptr;
+    copyQueue(queueToCopy);
+}
+
+template <class t>
+const LinkedQueue<t> &LinkedQueue<t>::operator=(const LinkedQueue<t> &queueToCopy)
+{
+    if (this != &queueToCopy)
+    {
+        copyQueue(queueToCopy);
+    }
+    return *this;
+}
+
+template <class t>
+LinkedQueue<t>::~LinkedQueue()
+{
+    initializeQueue();
+}
+
+template <class t>
+bool LinkedQueue<t>::isEmptyQueue() const
+{
+    return queueFront == nullptr;
+}
+
+template <class t>
+bool LinkedQueue<t>::isFullQueue() const
+{
+    return false;
+}
+
+template <class t>
+void LinkedQueue<t>::initializeQueue()
+{
+    if (!isEmptyQueue())
+    {
+        Node<t> *temp;
+        while (this->queueFront != nullptr)
+        {
+            temp = queueFront;
+            queueFront = queueFront->link;
+            delete temp;
+        }
+        queueRear = nullptr;
+    }
+}
+
+template <class t>
+t LinkedQueue<t>::front() const
+{
+    if (isEmptyQueue())
+    {
+        throw std::out_of_range("Cannot get the first item of an empty queue.");
+    }
+    return queueFront->data;
+}
+
+template <class t>
+t LinkedQueue<t>::back() const
+{
+    if (isEmptyQueue())
+    {
+        throw std::out_of_range("Cannot get the last item of an empty queue.");
+    }
+    return queueRear->data;
+}
 #endif
