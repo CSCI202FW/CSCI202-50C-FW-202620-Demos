28fff4093b917774889244410aa6e9096b284fa4
Angela Venable <avenable11@ivytech.edu>
2026-09-22 21:31:59 -0400

    before print tests

--- 09_22_26(binary Tree)/main.cpp
+++ 09_22_26(binary Tree)/main.cpp
@@ -8,6 +8,8 @@
 // to add a function to binary search tree
 // that prints the binary search tree in a tree shape
 
+void treeTest(BinarySearchTree<int> t);
+
 int main()
 {
     BinarySearchTree<int> tree;
@@ -27,7 +29,18 @@ int main()
             std::cerr << e.what() << '\n';
         }
     }
+    treeTest(tree);
     std::cout << tree.inorderTraversal() << std::endl;
+    std::cout << tree.preorderTraversal() << std::endl;
+    std::cout << tree.postorderTraversal() << std::endl;
+    std::cout << "Generated num: " << num << std::endl;
+    std::cout << "Tree Height: " << tree.treeHeight() << std::endl;
+    std::cout << "Tree Nodes: " << tree.treeNodeCount() << std::endl;
+    std::cout << "Tree Leaves: " << tree.treeLeavesCount() << std::endl;
 
     return 0;
+}
+
+void treeTest(BinarySearchTree<int> t)
+{
 }
